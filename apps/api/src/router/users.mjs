b852095573712c9660ecd7c7b1244b2185bf36cb
joaberch <122774888+joaberch@users.noreply.github.com>
2026-03-04 10:12:06 +0100

    refactor(backend user): remove useless route and remake other route
    Get all users route instead of getting students and teachers separately
    Update user instead of route updating only role
    Create user instead of creating by role

--- apps/api/src/router/users.mjs
+++ apps/api/src/router/users.mjs
@@ -13,7 +13,7 @@ usersRouter.get("/students", auth, authorizeRoles("admin", "teacher"), (req, res
     User.findAll({
         where: {
             role: "student",
-            isDeleted: false,
+            isDeleted: false, //TODO don't return hashedPassword
         }
     }).then((students) => {
         const message = "Tous les étudiants ont été récupérés.";
@@ -21,62 +21,33 @@ usersRouter.get("/students", auth, authorizeRoles("admin", "teacher"), (req, res
     });
 });
 
-//Get all teachers
-usersRouter.get("/teachers", auth, authorizeRoles("admin"), (req, res) => { //TODO - useful for teacher to get this info? In rapport
+//Get all users
+usersRouter.get("/", auth, authorizeRoles("admin"), (req, res) => {
     User.findAll({
-        where: {
-            role: "teacher",
-            isDeleted: false,
-        }
     }).then((teachers) => {
-        const message = "Tous les enseignants ont été récupérés.";
+        const message = "Tous les utilisateurs ont été récupérés."; //TODO don't return hashedPassword
         res.json(success(message, teachers));
     });
 });
 
-//Update user to students
-usersRouter.put("/student/:id", auth, authorizeRoles("admin"), async (req, res) => {
+//Update user
+usersRouter.put("/:id", auth, authorizeRoles("admin"), async (req, res) => {
     const userId = req.params.id;
-    let updatedUser = await User.findByPk(userId);
 
-    if (!updatedUser) {
-        return res.status(404).json({ message: "Utilisateur non trouvé" });
+    try {
+        const user = await User.findByPk(userId);
+    
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+    
+        await user.update(req.body); //TODO prevent hashedPassword to be edited?
+
+        const message = `L'utilisateur ${user.login} a été modifié.`;
+        res.json(success(message, user));
+    } catch (error) {
+        console.error("Erreur:", error)
     }
-
-    await updatedUser.update({ role: "student" }).then((_) => {
-        const message = `L'utilisateur ${updatedUser.login} est maintenant un étudiant.`;
-        res.json(success(message, updatedUser));
-    });
-});
-
-//Update user to teachers
-usersRouter.put("/teacher/:id", auth, authorizeRoles("admin"), async (req, res) => {
-    const userId = req.params.id;
-    let updatedUser = await User.findByPk(userId);
-
-    if (!updatedUser) {
-        return res.status(404).json({ message: "Utilisateur non trouvé" });
-    }
-
-    await updatedUser.update({ role: "teacher" }).then((_) => {
-        const message = `L'utilisateur ${updatedUser.login} est maintenant un enseignant.`;
-        res.json(success(message, updatedUser));
-    });
-});
-
-//Update user to admin
-usersRouter.put("/admin/:id", auth, authorizeRoles("admin"), async (req, res) => {
-    const userId = req.params.id;
-    let updatedUser = await User.findByPk(userId);
-
-    if (!updatedUser) {
-        return res.status(404).json({ message: "Utilisateur non trouvé" });
-    }
-
-    await updatedUser.update({ role: "admin" }).then((_) => {
-        const message = `L'utilisateur ${updatedUser.login} est maintenant un administrateur.`;
-        res.json(success(message, updatedUser));
-    });
 });
 
 //Archivate an user
@@ -94,20 +65,19 @@ usersRouter.put("/archivate/:id", auth, authorizeRoles("admin"), async (req, res
     });
 });
 
-//Create student
-usersRouter.post("/student", auth, authorizeRoles("admin"), async (req, res) => { //TODO - can teacher do?
+//Create user
+usersRouter.post("/", auth, authorizeRoles("admin"), async (req, res) => { //TODO - can teacher do?
     try {
-        const studentData = {
+        const userData = {
             ...req.body,
-            role: "student",
             isDeleted: false
         }
         if (!req.body.password) {
             return res.status(400).json({ message: "Mot de passe requis." });
         }
-        studentData.hashedPassword = await bcrypt.hash(req.body.password, 12);
+        userData.hashedPassword = await bcrypt.hash(req.body.password, 12);
 
-        const createdUser = await User.create(studentData);
+        const createdUser = await User.create(userData);
 
         const { hashedPassword, ...safeUser } = createdUser.toJSON();
         const message = `L'utilisateur' ${safeUser.name} a été créé.`;
@@ -119,31 +89,6 @@ usersRouter.post("/student", auth, authorizeRoles("admin"), async (req, res) => 
         const message = "L'utilisateur n'a pas été créé. Veuillez réessayer dans un moment.";
         res.status(500).json({ message, data: error.message });
     }
-});
-
-//Create teacher
-usersRouter.post("/teacher", auth, authorizeRoles("admin"), async (req, res) => {
-    const teacherData = {
-        ...req.body,
-        role: "teacher",
-        isDeleted: false
-    }
-    if (!req.body.password) {
-        return res.status(400).json({ message: "Mot de passe requis." });
-    }
-    teacherData.hashedPassword = await bcrypt.hash(req.body.password, 12); //TODO (no err just for better readability)
-
-    User.create(teacherData).then((createdUser) => {
-        const { hashedPassword, ...safeUser } = createdUser.toJSON(); //Don't return the hashedPassword to the frontend
-        const message = `L'utilisateur' ${safeUser.name} a été créé.`;
-        res.json(success(message, safeUser));
-    }).catch((error) => {
-        if (error instanceof ValidationError) {
-            return res.status(400).json({ message: error.message, data: error });
-        }
-        const message = "L'utilisateur n'a pas été créé. Veuillez réessayer dans un moment.";
-        res.status(500).json({ message, data: error });
-    })
 });
 
 export { usersRouter };
