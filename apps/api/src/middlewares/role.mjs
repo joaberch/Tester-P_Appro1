9d0d7658a0fc334dd4bb4fc29710721a3bbe5952
joaberch <122774888+joaberch@users.noreply.github.com>
2026-03-27 16:06:45 +0100

    add(api dependencies): jest

--- apps/api/src/middlewares/role.mjs
+++ apps/api/src/middlewares/role.mjs
@@ -3,7 +3,6 @@ import { getUser } from "../services/me.mjs";
 const authorizeRoles = (...allowedRoles) => {
     return async (req, res, next) => {
         const me = await getUser(req.user.oid) //TODO cleaner
-        console.log(me)
         if(!allowedRoles.includes(me.role)) {
             return res.status(403).json({ message: "Vous n'avez pas le rôle requis pour accéder à la ressource" } );
         }
