dfef2f6eee0d1585db35c0d05a8d3730b9f7b2ec
joaberch <122774888+joaberch@users.noreply.github.com>
2026-04-23 15:11:37 +0200

    ethan refactor

--- apps/api/src/controllers/objectives.mjs
+++ apps/api/src/controllers/objectives.mjs
@@ -1,21 +1,24 @@
 import * as objectivesService from "../services/objectives.mjs";
 
-export async function getObjectivesOfModule(req, res) {
-    const objectives = await objectivesService.getObjectivesOfModule(req.query.idModule);
-    res.status(200).json(objectives);
-}
 
-export async function createObjective(req, res) {
-    const objective = await objectivesService.createObjective(req.body);
-    res.status(201).json(objective);
-}
+export const ObjectiveController = {
+    get : async (req, res) =>{
+        const objectives = await objectivesService.getObjectivesOfModule(req.query.idModule);
+        res.status(200).json(objectives);
+    },
 
-export async function archiveObjective(req, res) {
-    const objective = await objectivesService.archiveObjective(req.params.id);
-    res.status(200).json(objective);
-}
+    create : async(req, res) =>{
+        const objective = await objectivesService.createObjective(req.body);
+        res.status(201).json(objective);
+    },
 
-export async function editObjective(req, res) {
-    const objective = await objectivesService.editObjective(req.params.id, req.body);
-    res.status(200).json(objective);
+    archive : async(req, res) =>{
+        const objective = await objectivesService.archiveObjective(req.params.id);
+        res.status(200).json(objective);
+    },
+
+    edit : async(req, res) =>{
+        const objective = await objectivesService.editObjective(req.params.id, req.body);
+        res.status(200).json(objective);
+    }
 }
--- apps/api/src/router/objectives.mjs
+++ apps/api/src/router/objectives.mjs
@@ -1,20 +1,19 @@
 import express from "express";
 import auth from "../middlewares/auth.mjs";
 import authorizeRoles from "../middlewares/role.mjs";
-import { archiveObjective, createObjective, editObjective, getObjectivesOfModule } from "../controllers/objectives.mjs";
-
+import { ObjectiveController as Objective } from "../controllers/objectives.mjs";
 const objectivesRouter = express();
 
 //Get all objectives of module
-objectivesRouter.get("/", auth, authorizeRoles("admin", "teacher", "student"), getObjectivesOfModule);
+objectivesRouter.get("/", auth, authorizeRoles("admin", "teacher", "student"), Objective.get);
 
 //Create an objective
-objectivesRouter.post("/", auth, authorizeRoles("admin", "teacher"), createObjective);
+objectivesRouter.post("/", auth, authorizeRoles("admin", "teacher"), Objective.create);
 
 //Archive an objective
-objectivesRouter.put("/archive/:id", auth, authorizeRoles("admin", "teacher"), archiveObjective);
+objectivesRouter.put("/archive/:id", auth, authorizeRoles("admin", "teacher"), Objective.archive);
 
 //Edit an objective
-objectivesRouter.put("/:id", auth, authorizeRoles("admin", "teacher"), editObjective);
+objectivesRouter.put("/:id", auth, authorizeRoles("admin", "teacher"), Objective.edit);
 
 export { objectivesRouter };
